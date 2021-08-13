f6ccf160ceb8dd12c2093899c65b6f1a74bda35e
Daniel <coding.dotping@gmail.com>
2021-08-13 15:18:10 +1000

    array slice sort

---
+++ sort.js
@@ -0,0 +1,14 @@
+function solution(array, commands) {
+    var answer = [];
+    var start;
+    var end;
+    for (i = 0; i < commands.length; i++) {
+        start = commands[i][0] - 1;
+        end = commands[i][1];
+        returnValue = commands[i][2] - 1;
+        answer.push(array.slice(start, end).sort()[returnValue])
+    }
+    return answer;
+}
+
+console.log(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
