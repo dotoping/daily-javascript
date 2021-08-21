f2072ef406085ce68330a2d9b54e083ea6fac868
Daniel <coding.dotping@gmail.com>
2021-08-21 18:56:38 +1000

    typescript - access modifier, getter, setter

--- class-typescript.js
+++ class-typescript.js
@@ -1,18 +1,36 @@
 // class in TypeScript
 var Employee = /** @class */ (function () {
-    function Employee() {
+    // private _fullName: string;
+    // age: number;
+    // jobTitle: string;
+    // hourlyRate: number;
+    // workingHoursPerWeek: number;
+    // taxRate: number;
+    function Employee(_fullName, _age, _jobTitle, hourlyRate, workingHoursPerWeek, taxRate) {
         var _this = this;
+        this._fullName = _fullName;
+        this._age = _age;
+        this._jobTitle = _jobTitle;
+        this.hourlyRate = hourlyRate;
+        this.workingHoursPerWeek = workingHoursPerWeek;
+        this.taxRate = taxRate;
         this.printEmployeeDetails = function () {
-            console.log(_this.fullName + " has job as a(an) " + _this.jobTitle + " and get paid $" + (_this.hourlyRate * _this.workingHoursPerWeek) * ((100 - _this.taxRate) / 100) + " per week.");
+            console.log(_this.fullName + " is a(an) " + _this.jobTitle + " and get paid $" + (_this.hourlyRate * _this.workingHoursPerWeek) * ((100 - _this.taxRate) / 100) + " per week.");
         };
     }
+    ;
+    Object.defineProperty(Employee.prototype, "fullName", {
+        get: function () {
+            return this._fullName;
+        },
+        set: function (value) {
+            this._fullName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Employee;
 }());
-var employee1 = new Employee();
-employee1.fullName = 'Daniel K';
-employee1.age = 33;
-employee1.jobTitle = 'Software Developer';
-employee1.hourlyRate = 40;
-employee1.workingHoursPerWeek = 38;
-employee1.taxRate = 25;
+var employee1 = new Employee('Daniel', 33, 'Junior Developer', 40, 35, 25);
+employee1.fullName = 'Hogwart';
 employee1.printEmployeeDetails();
--- class-typescript.ts
+++ class-typescript.ts
@@ -1,23 +1,36 @@
 // class in TypeScript
 class Employee {
-    fullName: string;
-    age: number;
-    jobTitle: string;
-    hourlyRate: number;
-    workingHoursPerWeek: number;
-    taxRate: number;
+    // private _fullName: string;
+    // age: number;
+    // jobTitle: string;
+    // hourlyRate: number;
+    // workingHoursPerWeek: number;
+    // taxRate: number;
+
+    constructor(
+        private _fullName: string,
+        private _age: number,
+        private _jobTitle: string,
+        public hourlyRate: number,
+        public workingHoursPerWeek: number,
+        public taxRate: number) { };
+
+    get fullName() {
+        return this._fullName;
+    }
+
+    set fullName(value: string) {
+        this._fullName = value;
+    }
+
     printEmployeeDetails = (): void => {
         console.log(
-            `${this.fullName} has job as a(an) ${this.jobTitle} and get paid $${(this.hourlyRate * this.workingHoursPerWeek) * ((100 - this.taxRate) / 100)} per week.`
+            `${this.fullName} is a(an) ${this.jobTitle} and get paid $${(this.hourlyRate * this.workingHoursPerWeek) * ((100 - this.taxRate) / 100)} per week.`
         );
     }
 }
 
-let employee1 = new Employee();
-employee1.fullName = 'Daniel K';
-employee1.age = 33;
-employee1.jobTitle = 'Software Developer';
-employee1.hourlyRate = 40;
-employee1.workingHoursPerWeek = 38;
-employee1.taxRate = 25;
+let employee1 = new Employee('Daniel', 33, 'Junior Developer', 40, 35, 25);
+
+employee1.fullName = 'Hogwart';
 employee1.printEmployeeDetails();
