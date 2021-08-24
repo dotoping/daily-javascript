422f235d3d50a3693fc2a26e59e90fd745eb6508
Daniel <coding.dotping@gmail.com>
2021-08-25 09:39:15 +1000

    read 2 number from console

--- boj-2588.js
+++ boj-2588.js
@@ -1,4 +1,3 @@
-
 var readline = require('readline');
 
 var input = [];
@@ -8,40 +7,34 @@ var rl = readline.createInterface({
     output: process.stdout
 });
 
-// rl.prompt();
-
 rl.on('line', (cmd) => {
     input.push(cmd);
-}).on('line', (cmd) => {
-    input.push(cmd);
-    rl.close();
+    if (input.length == 2) rl.close();
 });
 rl.on('close', () => {
-    // console.log(input.join('\n'));
+    const a = parseInt(input[0]);
+    const b = input[1];
+    const c = [];
+    const result = [];
+
+    b.split("").reverse().reduce((pre, curr) => {
+        pre = curr * a;
+        c.push(pre);
+        return pre;
+    }, a);
+
+    const total = c.reduce((pre, curr, idx) => {
+        if (idx === 0) { pre += curr * 1 }
+        else if (idx === 1) { pre += curr * 10 }
+        else if (idx === 2) { pre += curr * 100 }
+        return pre;
+    })
+
+    const final = [...c, total];
+
+    final.forEach((v) => {
+        console.log(v);
+    });
+
     process.exit();
 });
-
-// const a = parseInt("472");
-// const b = "385";
-// const c = [];
-// const result = [];
-
-// b.split("").reverse().reduce((pre, curr) => {
-//     pre = curr * a;
-//     c.push(pre);
-//     return pre;
-// }, a);
-
-// const total = c.reduce((pre, curr, idx) => {
-//     if (idx === 0) { pre += curr * 1 }
-//     else if (idx === 1) { pre += curr * 10 }
-//     else if (idx === 2) { pre += curr * 100 }
-//     return pre;
-// })
-
-// const final = [...c, total];
-
-// final.forEach((v) => {
-//     console.log(v);
-// });
-
