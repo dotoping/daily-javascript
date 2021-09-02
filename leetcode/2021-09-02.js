09eed53823bf382ce8d22dec6fe04d741910dba0
Daniel <coding.dotping@gmail.com>
2021-09-02 13:52:13 +1000

    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.

--- leetcode/2021-09-02.js
+++ leetcode/2021-09-02.js
@@ -28,5 +28,35 @@ var duplicateZeros = function (arr) {
     return arr;
 };
 
-console.log(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0]))
-console.log(duplicateZeros([1, 2,3]))
+// console.log(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0]))
+// console.log(duplicateZeros([1, 2, 3]))
+
+
+
+
+/**
+ * 
+ You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ */
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+    nums1.splice(m, n);
+    nums2.forEach((num) => nums1.push(num));
+    nums1.sort((a, b) => a - b);
+    console.log(nums1);
+};
+
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3) //Output: [1,2,2,3,5,6]
+merge([1], 1, [], 0)
+merge([0],0,[1],1)
