3f3e0af0be5cbc51958bb01a73ce0bc1f580f275
Daniel <coding.dotping@gmail.com>
2021-12-06 11:18:24 +1100

    Time Conversion

---
+++ HackerRank/TimeConversion.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+   inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+   inputString = inputString.split('\n');
+
+   main();
+});
+
+function readLine() {
+   return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(s) {
+   // Write your code here
+   let hh = s.slice(0, 2);
+   let hh24 = s.includes('PM')
+      ? hh == '12' ? hh : parseInt(hh) + 12
+      : hh == '12' ? '00' : hh;
+
+   let newS = hh24 + s.slice(2, 8);
+
+   return newS;
+}
+
+function main() {
+   const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+   const s = readLine();
+
+   const result = timeConversion(s);
+
+   ws.write(result + '\n');
+
+   ws.end();
+}
